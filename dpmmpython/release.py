88dcfcc8a25a40f6341f1dcaaf963e0bac3037d9
Or Dinari <dinari.or@gmail.com>
2022-08-05 20:55:47 +0300

    version

--- dpmmpython/release.py
+++ dpmmpython/release.py
@@ -1,5 +1,5 @@
 # This file is executed via setup.py and imported via __init__.py
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 # For Python versioning scheme, see:
 # https://www.python.org/dev/peps/pep-0440/#version-scheme
